e6b2d482aaedae03aac23096e9a61359df479a60
Attogram <barberi.office@gmail.com>
2016-07-14 09:00:22 +0200

    quote fix

--- sandbox.js
+++ sandbox.js
@@ -12,19 +12,6 @@ function changeColor(newColor)
     document.doodle.color_status.value = activeColor;
     showEvent("Change Color: " + activeColor);
 }
-
-function m(obj)
-{
-    if (isDrawing) {
-        obj.style.backgroundColor = activeColor;
-        showEvent(
-            "Mouseover: "
-            + " " + obj.cellIndex + "x?" // + obj
-            + " " + obj.offsetTop + "x" + obj.offsetLeft
-            + " " + obj.offsetWidth + "x" + obj.offsetHeight
-        );
-    }
-}
 function toggleDrawing()
 {
     if(isDrawing) {
@@ -42,16 +29,30 @@ function c(obj)
     toggleDrawing();
     m(obj);
 }
+function m(obj)
+{
+    if (isDrawing) {
+        obj.style.backgroundColor = activeColor;
+        showEvent(
+            "Mouseover: "
+            + " " + obj.cellIndex + "x?" // + obj
+            + " " + obj.offsetTop + "x" + obj.offsetLeft
+            + " " + obj.offsetWidth + "x" + obj.offsetHeight
+        );
+    }
+}
 function saveAsHTML()
 {
     canvas = document.getElementById("canvas");
-    out = "<table cellpadding="0" cellspacing="0" border="0" width="100" height="100">";
+    out = '<table cellpadding="0" cellspacing="0" border="0" width="100" height="100">';
     for( x = 0; x < canvas.rows.length; x++ ) {
         out += "<tr>";
         for( y = 0; y < canvas.rows[x].cells.length; y++ ) {
             clr = canvas.rows[x].cells[y].style.backgroundColor;
-            if( clr == "" ) { clr = "White"; }
-            out = out + "<td style="background-color:" + clr + "" >&nbsp;</td>";
+            if( clr == "" ) {
+                clr = "White";
+            }
+            out = out + '<td style="background-color:" + clr + "" >&nbsp;</td>';
         }
         out = out + "</tr>";
       }
